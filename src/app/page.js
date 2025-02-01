ab0506252fb49abf48adb79e9dc6a7601dcaac45
Anitha <anitha.gujarathi@vegastack.com>
2025-02-01 15:09:30 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>Hello, Welcome to Next JS</h1>
+      <h1>Hi, Welcome to Next JS</h1>
     </div>
   );
 }
